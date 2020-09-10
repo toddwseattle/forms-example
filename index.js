556f568464b12be8a1f7c283106c3428e555b06e
toddwseattle <todd@warrenfamily.org>
2020-09-09 22:46:43 -0700

    add some comments

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// Use DOM to insert text in an element
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Add User</h1>`;
 const form = document.getElementById("addForm");
+// write a function to connect to the listner
 function onSubmit(event) {
    event.preventDefault(); 
    const first= form.elements["f_name"].value;
    const last = form.elements["l_name"].value;
-   window.alert(`hey ${first} ${last} you submitted a form`);
+   window.alert(`hey ${first} ${last}  you submitted a form`);
 }
+// attach function to the form "submit" event.
 form.addEventListener('submit',onSubmit)
